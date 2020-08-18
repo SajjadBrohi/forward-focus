e2995e9836b14065ebd8edf1cef90c73c8ed8de8
SajjadBrohi <s.brohi@uq.net.au>
2020-08-19 04:42:12 +1000

    Insert new documment to database

--- packages/server/build/index.js
+++ packages/server/build/index.js
@@ -1,8 +1,37 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+
+// mongoose
+// 	.connect('mongodb://localhost/wikiDB', {
+// 		useUnifiedTopology: true,
+// 		useNewUrlParser: true,
+// 		useCreateIndex: true,
+// 	})
+// 	.then(() => {
+// 		console.log('Connected To Mongo Db DataBase');
+// 	})
+// 	.catch((err) => {
+// 		console.log('DataBase Connection Error ' + err);
+// 	});
+
+mongoose.connect('mongodb://localhost:27017/wikiDB', {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
+const articleSchema = new mongoose.Schema({
+	title: String,
+	content: String,
+});
+const Article = mongoose.model('Article', articleSchema);
+const mango = new Article({
+	title: 'Mango goes sour',
+	content: 'It is true that Mangoes go sour.',
+});
+mango.save();
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+	res.sendFile(__dirname + '/index.html');
 });
 app.listen(3000, () => {
-    console.log('Server started at port 3000.');
+	console.log('Server started at port 3000.');
 });
