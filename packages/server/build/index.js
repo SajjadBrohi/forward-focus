b331a3fa8f039e0d6f6e34cab311b90d6eb4d871
SajjadBrohi <s.brohi@uq.net.au>
2020-08-18 23:12:57 +0500

    Use mongoose to connect to database in mongoDB

--- packages/server/build/index.js
+++ packages/server/build/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const app = express();
-console.log(__dirname + '');
 app.get('/', (req, res) => {
-	res.sendFile();
+    res.sendFile(__dirname + '/index.html');
 });
 app.listen(3000, () => {
-	console.log('Server started at port 3000.');
+    console.log('Server started at port 3000.');
 });
--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,5 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+
+mongoose.connect('mongodb://localhost/wikiDB', {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
+
+const articleSchema = new mongoose.Schema({
+	title: String,
+	content: String,
+});
+
+const Article = mongoose.model('Article', articleSchema);
+
+const mango = new Article({
+	title: 'Mango goes sour',
+	content: 'It is true that Mangoes go sour.',
+});
+
+mango.save();
 
 app.get('/', (req, res): void => {
 	res.sendFile(__dirname + '/index.html');
