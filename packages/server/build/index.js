06b4b987b6ef83fc37fa9d62be1808fc12cfd0b2
Sajjad Brohi <s.brohi@uq.net.au>
2020-08-19 19:55:06 +1000

    Add GET, POST routes

--- packages/server/build/index.js
+++ packages/server/build/index.js
@@ -1,24 +1,35 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-
+app.use(express.urlencoded({ extended: true }));
 mongoose.connect('mongodb://localhost/wikiDB', {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
 });
-
 const articleSchema = new mongoose.Schema({
 	title: String,
 	content: String,
 });
 const Article = mongoose.model('Article', articleSchema);
-const mango = new Article({
-	title: 'Mango goes sour',
-	content: 'It is true that Mangoes go sour.',
-});
-mango.save();
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html');
+});
+app.get('/articles', (req, res) => {
+	Article.find({}, (err, foundArticles) => {
+		if (err) {
+			res.send(err);
+		} else {
+			res.send(foundArticles);
+		}
+	});
+});
+app.post('/articles', (req, res) => {
+	const article = new Article({
+		title: req.body.title,
+		content: req.body.content,
+	});
+
+	article.save();
 });
 app.listen(3000, () => {
 	console.log('Server started at port 3000.');
--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+
 mongoose.connect('mongodb://localhost/wikiDB', {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
@@ -16,6 +18,25 @@ const Article = mongoose.model('Article', articleSchema);
 
 app.get('/', (req, res): void => {
 	res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/articles', (req, res): void => {
+	Article.find({}, (err, foundArticles) => {
+		if (err) {
+			res.send(err);
+		} else {
+			res.send(foundArticles);
+		}
+	});
+});
+
+app.post('/articles', (req, res): void => {
+	const article = new Article({
+		title: req.body.title,
+		content: req.body.content,
+	});
+
+	article.save();
 });
 
 app.listen(3000, (): void => {
