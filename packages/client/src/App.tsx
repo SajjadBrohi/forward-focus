d16a8ce9794b9a70faa13b021e8194b6bd298f49
SajjadBrohi <s.brohi@uq.net.au>
2020-08-21 07:08:22 +1000

    Refactor code

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -3,14 +3,7 @@ import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Note from './components/Note';
-//4. Create a Footer.jsx component that renders a <footer> element
-//to show a copyright message in a <p> with a dynamically updated year.
-//5. Create a Note.jsx component to show a <div> element with a
-//<h1> for a title and a <p> for the content.
-//6. Make sure that the final website is styled like the example shown here:
-//https://l1pp6.csb.app/
 
-//HINT: You will need to study the classes in teh styles.css file to appy styling.
 function App() {
 	return (
 		<div>
--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -1,7 +1,5 @@
-//1. Create a new React app.
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(<App />, document.querySelector('#root'));
-//2. Create a App.jsx component.
