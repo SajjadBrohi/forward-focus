ed34dc9d8d55a9a3f7f0b7865e819f8873a0e1e4
SajjadBrohi <s.brohi@uq.net.au>
2020-08-22 22:19:51 +1000

    Add types for states

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Note from './components/Note';
 import CreateArea from './components/CreateArea';
-import notes from './Notes';
+// import notes from './Notes';
+
+interface Note {
+	key: number;
+	title: string;
+	content: string;
+}
+
 function App() {
+	const [notes, setNotes] = useState<Note[]>([]);
+	const [post, setPost] = useState({
+		title: '',
+		content: '',
+	});
+
+	function newPost(title: string, content: string) {
+		console.log(title, content);
+	}
+
 	return (
 		<div>
-			<Header /> <CreateArea /> <Footer />
+			<Header /> <CreateArea postFunction={newPost} /> <Footer />
 			{notes.map((note) => (
 				<Note id={note.key} title={note.title} content={note.content} />
 			))}
--- packages/client/src/components/CreateArea.tsx
+++ packages/client/src/components/CreateArea.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
 
-function CreateArea() {}
-
+function CreateArea(props: Function) {
+	return (
+		<div>
+			<form>
+				<input name="title" placeholder="Title" />
+				<textarea name="content" placeholder="Take a note..." rows={3} />
+				<button>Add</button>
+			</form>
+		</div>
+	);
+}
 export default CreateArea;
