d17c7bf207e5766b7534884f10ec340fc01fd296
Saj Brohi <s.brohi@uq.net.au>
2020-09-08 19:15:50 +1000

    Add starter text to scrapper.ts

--- packages/server/src/scrapper.ts
+++ packages/server/src/scrapper.ts
@@ -1,1 +1,64 @@
-const puppeteer = require('puppeteer');
+// Taken from the following link as a starter
+// https://www.lewuathe.com/simple-crawling-with-puppeteer-in-typescript.html
+
+import {URL} from 'url';
+import {mkdirSync, existsSync} from 'fs';
+import * as puppeteer from 'puppeteer';
+
+export class Crawer {
+  private baseUrl: string;
+
+  constructor(baseUrl: string) {
+    this.baseUrl = baseUrl;
+  }
+
+  crawl(site: any) {
+    (async () => {
+      // Wait for browser launching.
+     const browser = await puppeteer.launch();
+     // Wait for creating the new page.
+     const page = await browser.newPage();
+
+     await this.crawlInternal(page, `${this.baseUrl}/index.html`, site["children"], site["name"]);
+
+     browser.close();
+    })();
+  }
+
+  /**
+   * Crawling the site recursively
+   * selectors is a list of selectors of child pages.
+   */
+  async crawlInternal(page: any, path: string, selectors: [string], dirname: string) {
+    // Create a directory storing the result PDFs.
+    if (!existsSync(dirname)) {
+      mkdirSync(dirname);
+    }
+
+    // Go to the target page.
+    let url = new URL(path);
+    await page.goto(path, {waitUntil: 'networkidle'});
+    // Take a snapshot in PDF format.
+    await page.pdf({path:
+      `${dirname}/${url.pathname.slice(1).replace("/", "-")}.pdf`, format: 'A4'});
+    if (selectors.length == 0) {
+      return;
+    }
+
+       // Traversing in an order of BFS.
+    let items: [string] = await page.evaluate((sel) => {
+      let ret = [];
+       for (let item of document.querySelectorAll(sel)) {
+        let href = item.getAttribute("href");
+        ret.push(href);
+       }
+       return ret;
+    }, selectors[0]["selector"]);
+
+    for (let item of items) {
+      console.log(`Capturing ${item}`);
+      await this.crawlInternal(page,
+        `${item}`, selectors[0]["children"], `${dirname}/${selectors[0]["name"]}`)
+    }
+  }
+}
